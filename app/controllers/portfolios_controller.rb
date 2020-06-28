bea76efbe6abca158beca158e2b4c1b9d0cb931c
Jeff Safier <Jeff.Safier.gmail.com>
2020-06-28 00:54:54 -0400

    implemented destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,6 +35,15 @@ class PortfoliosController < ApplicationController
    	@portfolio = Portfolio.find(params[:id])
    end
 
+  def destroy
+  	@portfolio = Portfolio.find(params[:id])
+  	@portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+    end
+
+  end
+
   
   def update
 
